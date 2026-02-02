f5bb83951d5998ec9194674e09f5789cb36a355c
Joao Pedro <blvck.joao37@gmail.com>
2026-02-02 18:48:21 -0300

    Update - mais funções implementadas

--- Linked lists/Exercicios/q1.c
+++ Linked lists/Exercicios/q1.c
@@ -14,7 +14,7 @@ int comprimento(node* l) {
 
 
 int maiores_que_n(node* l, int n) {
-      int count = 0;
+    int count = 0;
 
     while (l != NULL) {
         if(l->info > n) { count++; }
@@ -47,3 +47,34 @@ node* concatena(node* l1, node* l2) {
     aux->next = l2;
     return l1;
 }
+
+node* retira_n(node* l, int n) {
+    node* result = NULL;
+
+    while (l != NULL) {
+        if(l->info > n) {
+            insertBack(&result, l->info);
+        }
+        l = l->next;
+    }
+
+    return result;
+}
+
+node* separa(node* l, int n) {
+    if (l == NULL) return NULL;
+
+    node* aux = l;
+
+    while (aux != NULL && aux->info != n) {
+        aux = aux->next;
+    }
+
+    if (aux == NULL) return NULL;
+
+    node* separada = aux->next;
+    aux->next = NULL;
+
+    return separada;
+}
+
